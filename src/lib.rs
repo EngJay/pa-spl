c04d5868f17d8c4fe6a5a35350c034e1a0ca3036
Jason Scott <jpscott07@gmail.com>
2024-07-07 11:24:42 -0400

    adjust to depend on embedded-hal ver 0.2.7 since that is what stm32f3xx-hal depends on

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #![cfg_attr(not(test), no_std)]
 
-use embedded_hal::i2c::I2c;
+use embedded_hal::blocking::i2c;
 
 /// PCB Artists SPL Module I2C address.
 const DEVICE_ADDR: u8 = 0x48;
@@ -18,10 +18,10 @@ const DEVICE_VER_MEMS_LTS_EXT: u8 = 0x81;
 /// Device Registers.
 const VER_REGISTER: u8 = 0x00;
 
-/// A PA SPL Module on the I2C bus `I2C`.
+/// A PA SPL Module on the I2C bus `I`.
 pub struct PaSpl<I2C>
 where
-    I2C: I2c,
+    I2C: i2c::Read + i2c::Write + i2c::WriteRead,
 {
     i2c: Option<I2C>,
 }
@@ -35,55 +35,50 @@ pub enum Error<E> {
     NoI2cInstance,
 }
 
-impl<I2C> PaSpl<I2C>
+impl<E, I2C> PaSpl<I2C>
 where
-    I2C: I2c,
+    I2C: i2c::Read<Error = E> + i2c::Write<Error = E> + i2c::WriteRead<Error = E>,
 {
     /// Initializes the PA SPL Module driver.
     pub fn new(i2c: I2C) -> Self {
         Self { i2c: Some(i2c) }
     }
 
-    pub fn read_firmware_version<E>(&mut self) -> Result<u8, Error<E>>
-    where
-        I2C: embedded_hal::i2c::I2c<Error = E>,
-    {
+    pub fn get_firmware_version(&mut self) -> Result<u8, Error<E>> {
         let mut ver = [0];
 
-        if let Some(ref mut i2c) = self.i2c {
-            i2c.write_read(DEVICE_ADDR, &[VER_REGISTER], &mut ver)
-                .map_err(Error::I2c)?;
-            Ok(ver[0])
-        } else {
-            // Return an appropriate error if I2C is None.
-            Err(Error::NoI2cInstance)
-        }
+        self.i2c
+            .as_mut()
+            .ok_or(Error::NoI2cInstance)?
+            .write_read(DEVICE_ADDR, &[VER_REGISTER], &mut ver)
+            .map_err(Error::I2c)?;
+        Ok(ver[0])
+
+        // if let Some(ref mut i2c) = self.i2c {
+        //     i2c.write_read(DEVICE_ADDR, &[VER_REGISTER], &mut ver)
+        //         .map_err(Error::I2c)?;
+        //     Ok(ver[0])
+        // } else {
+        //     // Return an appropriate error if I2C is None.
+        //     Err(Error::NoI2cInstance)
+        // }
     }
 
     /// Destroys this driver and releases the I2C bus.
     pub fn destroy(mut self) -> I2C {
-        self.i2c.take().expect("I2C instance already taken")
-    }
-}
-
-impl<I2C> Drop for PaSpl<I2C>
-where
-    I2C: I2c,
-{
-    fn drop(&mut self) {
-        if let Some(_i2c) = self.i2c.take() {
-            // Additional clean-up, logging, etc. here.
-        }
+        self.i2c
+            .take()
+            .expect("I2C instance has already been taken")
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::{PaSpl, DEVICE_ADDR, DEVICE_VER_MEMS_LTS_ASA, VER_REGISTER};
-    use embedded_hal_mock::eh1::i2c::{Mock as I2cMock, Transaction as I2cTransaction};
+    use embedded_hal_mock::eh0::i2c::{Mock as I2cMock, Transaction as I2cTransaction};
 
     #[test]
-    fn read_firmware_version() {
+    fn firmware_version() {
         let expectations = vec![I2cTransaction::write_read(
             DEVICE_ADDR,
             vec![VER_REGISTER],
@@ -92,7 +87,7 @@ mod tests {
         let i2c_mock = I2cMock::new(&expectations);
 
         let mut pa_spl = PaSpl::new(i2c_mock);
-        let version = pa_spl.read_firmware_version().unwrap();
+        let version = pa_spl.get_firmware_version().unwrap();
         assert_eq!(DEVICE_VER_MEMS_LTS_ASA, version);
 
         let mut mock = pa_spl.destroy();
