c24003e83e514fe0023d52b33f4314b39724a3d3
Jason Scott <jpscott07@gmail.com>
2024-07-12 10:45:12 -0400

    avoid fixed vlaue in comment

--- examples/read-decibel-value/src/main.rs
+++ examples/read-decibel-value/src/main.rs
@@ -170,6 +170,8 @@ fn main() -> ! {
                 asm::nop(); // If real app, replace with actual error handling.
             }
         });
-        delay.delay_ms(UART_WRITE_DELAY_MS); // Limit algorithm to 2 Hz.
+
+        // Limit algorithm to (1000 * (1 / UART_WRITE_DELAY_MS)) Hz.
+        delay.delay_ms(UART_WRITE_DELAY_MS);
     }
 }
